8dea6b960c2077f70263a22ae7b0db00a14a0986
Neil Woodhouse <clonesupreme@gmail.com>
2022-01-17 01:54:48 +0000

    Migrated to using JSON message format for now

--- Development/2. Network Prototyping/client.py
+++ Development/2. Network Prototyping/client.py
@@ -1,21 +1,28 @@
 import asyncio
 import time
+import json
 from websockets import connect
 
 async def send_message(uri, message, username):
-    data_format = "{0} - {1}: {2}"
     display_format = "{0}: {1}"
     send_time  = time.time()
 
+    data = {
+        "username": username, 
+        "timestamp": send_time,
+        "message": message, 
+    }
+
     async with connect(uri) as endpoint:
-        await endpoint.send(data_format.format(send_time, username, message))
+
+        await endpoint.send(json.dumps(data))
         print(display_format.format(username, message))
         response = await endpoint.recv()
         print(response)
 
 def start():
-    #URI = "ws://localhost:8765"
-    URI = "ws://192.168.0.62:80/ws"
+    URI = "ws://localhost:8765"
+    # URI = "ws://192.168.0.62:80/ws"
     welcome_string = "Welcome to LoRa Talk!"
     greeting_string = "Hi {}. Enter 'q' or 'quit' to exit. Enter anything else to send a message."
     exit_string = "Goodbye {}"
