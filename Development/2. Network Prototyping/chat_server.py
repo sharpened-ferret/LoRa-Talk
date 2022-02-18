1f91a650b1204168a89d4221a021ff4153213982
Neil Woodhouse <clonesupreme@gmail.com>
2022-02-18 22:41:11 +0000

    Rebuilt websocket handling in chat_server

--- Development/2. Network Prototyping/chat_server.py
+++ Development/2. Network Prototyping/chat_server.py
@@ -1,4 +1,5 @@
 import asyncio
+from contextlib import nullcontext
 import websockets
 import json
 import sqlite3
@@ -9,22 +10,79 @@ connected = set()
 
 DATABASE_PATH = "chat.db"
 
-async def handler(websocket):
-    # connected.add(websocket)
-    # print(connected)
-    async for message in websocket:
-        print(message)
-        await recieve_msg(message)
-        response = message
-        # websockets.broadcast(connected, response)
-        await websocket.send(response)
+current_msg = ""
+
+# async def handler(websocket, path):
+#     connected.add(websocket)
+#     # print(connected)
+#     async for message in websocket:
+#         print(message)
+#         await recieve_msg(message)
+#         response = message
+#         # websockets.broadcast(connected, response)
+#         for client in connected:
+#             try:
+#                 await client.send(response)
+#                 print("Send message to {}".format(client))
+#             except:
+#                 print("Failed to Send message to {}".format(client))
+
+async def handler(websocket, path):
+    global connected
+    if (websocket not in connected):
+        connected.add(websocket)
+
+        print("New Connection: {}".format(str(websocket)))
+        try:
+            while True:
+                reciever_task = asyncio.ensure_future(websocket.recv())
+                sender_task = asyncio.ensure_future(send_msg(websocket, path))
+                done, pending = await asyncio.wait(
+                    [reciever_task, sender_task],
+                    return_when=asyncio.FIRST_COMPLETED
+                )
+
+                if reciever_task in done:
+                    message = reciever_task.result()
+                    await recieve_msg(message)
+                else:
+                    reciever_task.cancel()
+
+                if sender_task in done:
+                    message = sender_task.result()
+                    #await send_msg(websocket, path)
+                    if (message != None):
+                        print(message)
+                        await websocket.send(str(message))
+                else:
+                    print("Cancelled Send")
+                    sender_task.cancel()
+                    
+        finally:
+            print("Exception Triggered:")
+            connected.remove(websocket)
+
+async def send_msg(websocket, path):
+    global current_msg
+    if (current_msg != ""):
+        #print(current_msg)
+        await websocket.send(current_msg)
+        returnMessage = current_msg
+        current_msg = ""
+        return returnMessage
+
+
 
 async def recieve_msg(message):
+    global current_msg
+    print("Recieved: " + message)
+    current_msg = message
     data = json.loads(message)
 
     cur = con.cursor()
     cur.execute("INSERT INTO messages(username, timestamp, message) VALUES (?, ?, ?)", (data["username"], data["timestamp"], data["message"]))
     con.commit()
+
 
 async def socket_manager():
     async with serve(handler, "localhost", 8765):
